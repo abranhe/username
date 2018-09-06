936e7b3c0bee2af6b41eea7b663abff35ee01f01
Abraham Hernandez <abraham@abranhe.com>
2018-09-06 11:03:48 -0400

    support for Python 2

--- username/__init__.py
+++ username/__init__.py
@@ -1,8 +1,10 @@
 import sys
 import os
 
-class u():
-	def __call__(self):
-		return os.path.split(os.path.expanduser('~'))[-1]
+class Username():
+	user = os.path.split(os.path.expanduser('~'))[-1]
 
-sys.modules[__name__] = u()
+	def __call__(self):
+		return  self.user
+
+sys.modules[__name__] = Username()
